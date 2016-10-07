9a23ae525763e0d7fada06a9c1a60b5ad138d9a6
Daniel Acedo Calderón <danielexmachina0451@gmail.com>
2016-10-07 14:27:59 +0200

    Mejora de sincronizacion del temporizador y control de estados

--- app/src/main/java/com/danielacedo/tema1/Ejercicio4Activity.java
+++ app/src/main/java/com/danielacedo/tema1/Ejercicio4Activity.java
@@ -7,8 +7,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Ejercicio4Activity extends AppCompatActivity {
+
+    private final int MILLIS_OFSET = 100; //Offset applied to the timer to be accurate
 
     private TextView txv_CoffeeCounter, txv_TimeCounter;
     private Button btn_AddTime, btn_SubstractTime, btn_StartCounter, btn_Pause;
@@ -16,6 +19,7 @@ public class Ejercicio4Activity extends AppCompatActivity {
     private CoffeeCounter coffeeCounter;
 
     protected boolean isPaused;
+    protected boolean isRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +56,7 @@ public class Ejercicio4Activity extends AppCompatActivity {
         btn_StartCounter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startTimer();
+                handleStartButton();
             }
         });
 
@@ -62,11 +66,44 @@ public class Ejercicio4Activity extends AppCompatActivity {
                 pauseTimer();
             }
         });
+        btn_Pause.setEnabled(false);
     }
 
     public void startTimer(){
-        coffeeTimer = new CoffeeTimer(coffeeCounter.toMillis(), 1000);
+        coffeeTimer = new CoffeeTimer(coffeeCounter.toMillis()+MILLIS_OFSET, 1000);
         coffeeTimer.start();
+        btn_Pause.setEnabled(true);
+        btn_Pause.setText(getResources().getString(R.string.btn_Pause));
+        btn_StartCounter.setText(getResources().getString(R.string.btn_StartCounter_Cancel));
+        isRunning = true;
+    }
+
+    public void handleStartButton(){
+        if(isRunning){
+            if(coffeeTimer!=null){
+                coffeeTimer.cancel();
+            }
+
+            coffeeTimer = null;
+            isPaused = false;
+            isRunning = false;
+            coffeeCounter.resetTime();
+            btn_Pause.setText(getResources().getString(R.string.btn_Pause));
+            btn_StartCounter.setText(getResources().getString(R.string.btn_StartCounter));
+            btn_Pause.setEnabled(false);
+            btn_AddTime.setEnabled(true);
+            btn_SubstractTime.setEnabled(true);
+            refreshTimerDisplay();
+        }else{
+            if(coffeeCounter.getMinutesCountDown() == 0 && coffeeCounter.getSecondsCountDown() == 0){
+                Toast.makeText(Ejercicio4Activity.this, getResources().getString(R.string.err_Timer_Start_Zero), Toast.LENGTH_SHORT).show();
+            }else{
+                startTimer();
+                btn_AddTime.setEnabled(false);
+                btn_SubstractTime.setEnabled(false);
+            }
+
+        }
     }
 
     public void refreshTimerDisplay(){
@@ -79,7 +116,7 @@ public class Ejercicio4Activity extends AppCompatActivity {
 
     public void pauseTimer(){
         if(isPaused){
-            coffeeTimer = new CoffeeTimer(coffeeCounter.toMillis(), 1000);
+            coffeeTimer = new CoffeeTimer(coffeeCounter.toMillis()+MILLIS_OFSET, 1000);
             coffeeTimer.start();
             isPaused = false;
             btn_Pause.setText(getResources().getString(R.string.btn_Pause));
@@ -95,6 +132,13 @@ public class Ejercicio4Activity extends AppCompatActivity {
     public void timerFinished(){
         txv_TimeCounter.setText(Ejercicio4Activity.this.getResources().getString(R.string.txv_TimeCounter_finished));
         coffeeCounter.addCoffee();
+        btn_Pause.setEnabled(false);
+        btn_AddTime.setEnabled(true);
+        btn_SubstractTime.setEnabled(true);
+        btn_Pause.setText(getResources().getString(R.string.btn_Pause));
+        btn_StartCounter.setText(getResources().getString(R.string.btn_StartCounter));
+        coffeeCounter.resetTime();
+        isRunning = false;
         refreshCoffeeCount();
     }
 
@@ -114,8 +158,6 @@ public class Ejercicio4Activity extends AppCompatActivity {
         @Override
         public void onFinish() {
             timerFinished();
-            coffeeCounter.resetTime();
-
         }
     }
 }
